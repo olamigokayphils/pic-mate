08809b580bf2138fec4d2344317cb791ccaf7523
Olamigoke Philip <olamigokayphils@gmail.com>
2020-11-21 13:22:15 +0100

    init local push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react"
 import './App.css';
+import axios from "axios"
+
+
 
 function App() {
+  const [images, setImages] = useState([])
+  const [imageLoaded, setImageLoaded] = useState(false)
+
+  useEffect(() => {
+    getImages()
+    //Callback doesn;t update state
+  }, [])
+
+  const getImages = () => {
+      axios.get(
+        "https://api.pexels.com/v1/search?query=people",
+        { headers: 
+          {
+             "Authorization": "563492ad6f917000010000016479bc398c6e434291e51216ac248167",
+             "Content-Type": "application/json"
+          }
+        }).then((res)=> {
+          setImages(res.data.photos)
+          setImageLoaded(true)
+
+        console.log(res.data.photos)
+        return res.data.photos
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h3 className="intro-text">Welcome to Pic-Mate</h3>
+      {/** If image is not loaded */}
+      {!imageLoaded && <h1 style={{ textAlign: "center", marginTop: "30px" }}>Loading...</h1>}
+
+    <div className="image-container">
+      {/** If image is Loaded; Render ++ */}
+      {images.map((image) => {
+        return (
+            <img className="image" src={image.src.medium}/>
+        )
+      })}
+      </div>
+
     </div>
   );
 }
